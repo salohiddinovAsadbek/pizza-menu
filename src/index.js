4b4c18e5019644f4c656bd13ad512762f4d8ea7c
salohiddinovAsadbek <mrxcapitain@gmail.com>
2025-01-09 10:52:55 +0500

    project ended

--- src/index.js
+++ src/index.js
@@ -57,20 +57,6 @@ function App() {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
-  return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
-      <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingridients}</p>
-        <span>{props.pizzaObj.price}</span>
-      </div>
-    </li>
-  );
-}
-
 function Header() {
   return (
     <header className="header">
@@ -80,14 +66,31 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 cretaive dishes to choose from. All
+            from our stone even, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+
       {/* <Pizza
         Pizza
         name="Pizza Spinaci"
@@ -104,11 +107,50 @@ function Menu() {
     </main>
   );
 }
+
+function Pizza({ pizzaObj }) {
+  // if (pizzaObj.soldOut) return null;
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(openHour);
+
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen ? (
+        <Order closeHours={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
     </footer>
+  );
+}
+
+function Order({ closeHours, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 until until {closeHours}:00. Come visit us
+        or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
